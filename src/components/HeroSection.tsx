572f12c2f92562c98bd6a93f5ae54aa7f0a9c219
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-05-22 23:21:17 +0545

    first commit

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { TypewriterEffect } from "./ui/typewriter-effect";
+
+export function TypewriterEffectDemo() {
+  const words = [
+    {
+      text: "Web Developer.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem] ">
+      <p className="text-center text-neutral-600 dark:text-neutral-200 text-base  mb-10 mx-8 sm:text-2xl md:text-3xl">
+        Hi there , I am Samrat Neupane.
+      </p>
+      <TypewriterEffect words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Resume
+        </button>
+      </div>
+    </div>
+  );
+}
